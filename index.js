21b597d21d6b44230f72ca630945f08c7de027f1
Jonathon Blom <blomjonathon@gmail.com>
2024-01-26 14:52:17 -0600

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// require in needed files, inquirer, and fs
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Circle = require("./lib/circle");
@@ -6,6 +8,7 @@ const Triangle = require("./lib/triangle");
 
 const shapeOptions = ['circle', 'triangle', 'square']
 
+// create inquirer prompts for input from the user 
 inquirer
   .prompt([
     {
@@ -40,7 +43,7 @@ inquirer
     const newSquare = new Square(response.text, response.textColor, response.shape, response.shapeColor)
 
     let newShape
-
+    // logic for shapes to be determined and stored into variable newShape
     if(response.shape === 'circle') {
       newShape = newCircle.renderCircle()
     } else if (response.shape === 'triangle'){
@@ -48,7 +51,7 @@ inquirer
     } else if (response.shape === 'square'){
       newShape = newSquare.renderSquare()
     } 
-  
+    // write file that generates logo.svg and displays that it worked
     fs.writeFile(
       "logo.svg",
       `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
